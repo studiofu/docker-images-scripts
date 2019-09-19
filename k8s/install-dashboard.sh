0660262db750a3191417bf2e3765bb389db02a40
origin <origin@k8s-docker-1.192.168.137.41.nip.io>
2019-09-19 15:12:45 +0800

    add basic docker images

---
+++ k8s/install-dashboard.sh
@@ -0,0 +1,7 @@
+#kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml
+
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-beta4/aio/deploy/recommended.yaml
+
+
+
