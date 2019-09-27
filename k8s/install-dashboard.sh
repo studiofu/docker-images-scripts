e52fff18ee1a222f5868df90db346ea8cd2f580d
Johnson Fu <johnson.fu@gmail.com>
2019-09-27 16:13:57 +0800

    update

--- k8s/install-dashboard.sh
+++
@@ -1,7 +0,0 @@
-#kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml
-
-
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-beta4/aio/deploy/recommended.yaml
-
-
-
