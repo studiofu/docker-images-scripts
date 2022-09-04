e74a74faf54049e7e995a8c36d887f32dfb864bf
Johnson Fu <johnson.fu@gmail.com>
2022-09-05 00:02:27 +0800

    update

--- dc-rabbitmq/run.sh
+++ dc-rabbitmq/run.sh
@@ -1,1 +1,1 @@
-docker run -d --hostname my-rabbit --name some-rabbit -e RABBITMQ_DEFAULT_USER=guest -e RABBITMQ_DEFAULT_PASS=guest -p 15672:15672 -p 5672:5672 rabbitmq:3-management
+docker run --restart=always -d --hostname my-rabbit --name some-rabbit -e RABBITMQ_DEFAULT_USER=guest -e RABBITMQ_DEFAULT_PASS=guest -p 15672:15672 -p 5672:5672 rabbitmq:3-management
